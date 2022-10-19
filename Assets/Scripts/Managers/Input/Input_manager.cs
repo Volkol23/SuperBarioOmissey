c8d3d39b29c68ac28f506122fdf9c54981434ca7
Volkol23 <adriguiu97@gmail.com>
2022-10-19 13:19:29 +0200

    Player + (WIP)Camera Movement

---
+++ Assets/Scripts/Managers/Input/Input_manager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Input_manager : MonoBehaviour
+{
+    public static Input_manager _INPUT_MANAGER;
+
+    private PlayerInputActions playerInputs;
+
+    private float timeSinceJumpPressed = 0f;
+    private Vector2 leftAxisValue = Vector2.zero;
+    private Vector2 rightAxisValue = Vector2.zero;
+
+    private void Awake()
+    {
+        if(_INPUT_MANAGER != null && _INPUT_MANAGER!= this)
+        {
+            Destroy(_INPUT_MANAGER);
+        }
+        else
+        {
+            playerInputs = new PlayerInputActions();
+            playerInputs.Character.Enable();
+
+            playerInputs.Character.Jump.performed += JumpButtonPressed;
+            playerInputs.Character.Move.performed += LeftAxisValue;
+            playerInputs.Character.CameraRotate.performed += RightAxisValue;
+
+            _INPUT_MANAGER = this;
+            DontDestroyOnLoad(_INPUT_MANAGER);
+        }
+    }
+
+    private void Update()
+    {
+        timeSinceJumpPressed += Time.deltaTime;
+
+        InputSystem.Update();
+    }
+
+    private void JumpButtonPressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("JumpButtonPressed");
+        timeSinceJumpPressed = 0f;
+    }
+
+    private void LeftAxisValue(InputAction.CallbackContext context)
+    {
+        leftAxisValue = context.ReadValue<Vector2>();
+        //Debug.Log("Magnitude: " + leftAxisValue.magnitude);
+        //Debug.Log("Normalize: " + leftAxisValue.normalized);
+    }
+
+    private void RightAxisValue(InputAction.CallbackContext context)
+    {
+        rightAxisValue = context.ReadValue<Vector2>();
+    }
+
+    public bool GetJumpButtonPressed()
+    {
+        return timeSinceJumpPressed == 0f;
+    }
+
+    public Vector2 GetLeftAxisValue()
+    {
+        return leftAxisValue;
+    }
+
+    public Vector2 GetRightAxisValue()
+    {
+        return rightAxisValue;
+    }
+}
--- Assets/Scripts/Player/Player_behaviour.cs
+++ Assets/Scripts/Player/Player_behaviour.cs
@@ -4,36 +4,92 @@ using UnityEngine;
 
 public class Player_behaviour : MonoBehaviour
 {
+    [Header("Movement Variables")]
     [SerializeField]
-    private float speed;
+    private float speed = 5f;
 
     [SerializeField]
-    private float jump_speed;
-    // Start is called before the first frame update
-    void Start()
+    private float jumpSpeed = 8f;
+
+    [SerializeField]
+    private float gravity = 20f;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private Camera mainCamera;
+
+    private CharacterController controller;
+    private Rigidbody playerRigidbody;
+
+    private Vector3 finalSpeed = Vector3.zero;
+    private Vector3 direction = Vector3.zero;
+
+    private Vector3 finalRotation = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
+        playerRigidbody = GetComponent<Rigidbody>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    currentSpeed += acceleration * Time.deltaTime;
-        //}
-        //else
-        //{
-        //    currentSpeed -= acceleration * Time.deltaTime;
-        //}
+        MoveCharacter();
+        RotateCharacter();
+    }
+    private void MoveCharacter()
+    {
+        //Read Input Manager Values Direction
+        Vector2 directionInput = Input_manager._INPUT_MANAGER.GetLeftAxisValue();
+        if (directionInput != null)
+        {
+            direction = directionInput.y * mainCamera.transform.forward + directionInput.x * mainCamera.transform.right;
+        }
 
-        //currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+        direction.Normalize();
 
-        //transform.position += transform.forward * currentSpeed * Time.deltaTime;
+        finalSpeed.x = direction.x * speed;
+        finalSpeed.z = direction.z * speed;
+
+        //Apply gravity
+        direction.y = -1f;
+
+        if (controller.isGrounded)
+        {
+            if (Input_manager._INPUT_MANAGER.GetJumpButtonPressed())
+            {
+                finalSpeed.y = jumpSpeed;
+            }
+            else
+            {
+                finalSpeed.y = direction.y * gravity * Time.deltaTime;
+            }
+        }
+        else
+        {
+            finalSpeed.y += direction.y * gravity * Time.deltaTime;
+        }
+
+        controller.Move(finalSpeed * Time.deltaTime);
     }
 
-    public float GetCurrentSpeed()
+    private void RotateCharacter()
     {
-        return this.speed;
+        Vector2 rotateInput = Input_manager._INPUT_MANAGER.GetRightAxisValue();
+        if(rotateInput != null)
+        {
+            rotationX = rotateInput.y;
+            rotationY = rotateInput.x;
+        }
+        
+        rotationX = Mathf.Clamp(rotationX, -50f, 50f);
+
+        transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
+        //transform.position
     }
 }
